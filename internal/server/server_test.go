cb078d81799f47400ff0c96c37d45461865dedb2
Oleksandr Zinchenko <zinchenko@live.com>
2020-06-16 23:02:18 -0700

    Tests for grpc server

---
+++ internal/server/server_test.go
@@ -0,0 +1,135 @@
+package server
+
+import (
+	"context"
+	"io/ioutil"
+	"net"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	api "github.com/weak-head/proglog/api/v1"
+	"github.com/weak-head/proglog/internal/log"
+	"google.golang.org/grpc"
+)
+
+func TestServer(t *testing.T) {
+	for scenario, fn := range map[string]func(
+		t *testing.T,
+		client api.LogClient,
+		config *Config,
+	){
+		"produce/consume a message to/from the log succeeds": testProduceConsume,
+		"produce/consume stream succeeds":                    testProduceConsumeStream,
+		"consume past log boundary fails":                    testConsumePastBoundary,
+	} {
+		t.Run(scenario, func(t *testing.T) {
+			client, config, teardown := testSetup(t, nil)
+			defer teardown()
+			fn(t, client, config)
+		})
+	}
+}
+
+func testSetup(t *testing.T, fn func(*Config)) (
+	client api.LogClient,
+	config *Config,
+	teardown func(),
+) {
+	t.Helper()
+
+	l, err := net.Listen("tcp", ":0")
+	require.NoError(t, err)
+
+	clientOptions := []grpc.DialOption{grpc.WithInsecure()}
+	cc, err := grpc.Dial(l.Addr().String(), clientOptions...)
+	require.NoError(t, err)
+
+	dir, err := ioutil.TempDir(os.TempDir(), "server_test")
+	require.NoError(t, err)
+
+	clog, err := log.NewLog(dir, log.Config{})
+	require.NoError(t, err)
+
+	config = &Config{
+		CommitLog: clog,
+	}
+	if fn != nil {
+		fn(config)
+	}
+
+	server, err := NewGRPCServer(config)
+	require.NoError(t, err)
+
+	go func() {
+		server.Serve(l)
+	}()
+
+	client = api.NewLogClient(cc)
+
+	return client, config, func() {
+		server.Stop()
+		cc.Close()
+		l.Close()
+	}
+
+}
+
+func testProduceConsume(
+	t *testing.T,
+	client api.LogClient,
+	config *Config,
+) {
+	ctx := context.Background()
+	want := &api.Record{
+		Value: []byte("hello world"),
+	}
+
+	produce, err := client.Produce(
+		ctx,
+		&api.ProduceRequest{
+			Record: want,
+		},
+	)
+	require.NoError(t, err)
+
+	consume, err := client.Consume(ctx, &api.ConsumeRequest{
+		Offset: produce.Offset,
+	})
+	require.NoError(t, err)
+	require.Equal(t, want, consume.Record)
+}
+
+func testProduceConsumeStream(
+	t *testing.T,
+	client api.LogClient,
+	config *Config,
+) {
+}
+
+func testConsumePastBoundary(
+	t *testing.T,
+	client api.LogClient,
+	config *Config,
+) {
+	ctx := context.Background()
+	produce, err := client.Produce(ctx, &api.ProduceRequest{
+		Record: &api.Record{
+			Value: []byte("hello world"),
+		},
+	})
+	require.NoError(t, err)
+
+	consume, err := client.Consume(ctx, &api.ConsumeRequest{
+		Offset: produce.Offset + 1,
+	})
+	if consume != nil {
+		t.Fatal("consume not nil")
+	}
+
+	got := grpc.Code(err)
+	want := grpc.Code((&api.ErrOffsetOutOfRange{}).GRPCStatus().Err())
+	if got != want {
+		t.Fatalf("got err: %v, want: %v", got, want)
+	}
+}
